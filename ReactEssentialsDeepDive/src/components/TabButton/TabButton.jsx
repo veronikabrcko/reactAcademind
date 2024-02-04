d6515d15f5b028de462380d9a09a667bdce0d66e
Veronika Brcko <vbrcko@dickeys.com>
2024-02-04 14:08:00 +0100

    Essentials deep dive initial commit (copy of essentials)

---
+++ ReactEssentialsDeepDive/src/components/TabButton/TabButton.jsx
@@ -0,0 +1,11 @@
+import './TabButton.css';
+
+export default function TabButton({ children, onSelect, isSelected }) {
+  return (
+    <li>
+      <button className={isSelected ? 'active' : undefined} onClick={onSelect}>
+        {children}
+      </button>
+    </li>
+  );
+}
