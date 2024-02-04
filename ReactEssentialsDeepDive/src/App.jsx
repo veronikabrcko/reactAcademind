d7df691124da576f673c200ccd7e6f4164d24aed
Veronika Brcko <vbrcko@dickeys.com>
2024-02-04 14:23:10 +0100

    removed div - fragment

--- ReactEssentialsDeepDive/src/App.jsx
+++ ReactEssentialsDeepDive/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -71,7 +71,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
