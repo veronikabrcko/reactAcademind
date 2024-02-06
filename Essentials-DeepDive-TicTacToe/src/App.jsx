847fbeecc2458dde3f9d317589e9d04d3d95d504
Veronika Brcko <vbrcko@dickeys.com>
2024-02-06 07:33:25 +0100

    added change activePlayerSymbol on SelectSquare

--- Essentials-DeepDive-TicTacToe/src/App.jsx
+++ Essentials-DeepDive-TicTacToe/src/App.jsx
@@ -1,15 +1,34 @@
+import { useState } from 'react';
+
 import Player from './components/Player.jsx';
 import GameBoard from './components/GameBoard.jsx';
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Veronika" symbol="X"></Player>
-          <Player initialName="Eduardo" symbol="O"></Player>
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === 'X'}
+          ></Player>
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === 'O'}
+          ></Player>
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
       Log
     </main>
