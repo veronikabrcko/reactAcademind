7d03f5dd30f2745a784b5eb17e6f43cbeabdc879
Veronika Brcko <vbrcko@dickeys.com>
2024-02-06 08:44:22 +0100

    disabled multiple button clicks

--- Essentials-DeepDive-TicTacToe/src/components/GameBoard.jsx
+++ Essentials-DeepDive-TicTacToe/src/components/GameBoard.jsx
@@ -36,7 +36,10 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button
+                  disabled={playerSymbol != null}
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                >
                   {playerSymbol}
                 </button>
               </li>
